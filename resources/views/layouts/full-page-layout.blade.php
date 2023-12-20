5659f436112a75e30fc98c44e77f0956c536647c
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-12-20 12:57:24 +0500

    design complete

--- resources/views/layouts/full-page-layout.blade.php
+++ resources/views/layouts/full-page-layout.blade.php
@@ -2,12 +2,16 @@
     <livewire:pages.campus-information-card/>
     <x-shared.general.main class="mb-5">
         <livewire:pages.page-navigation/>
-        <div class="row mt-3 mx-0 px-0">
+        <!-- <div class="row mt-3 mx-0 px-0"> -->
             <!-- Main Content -->
-            <div class="col-12 px-0">
+            <!-- <div class="col-12 px-0"> -->
+                <main class="main-wrapper">
+                    <div class="main-content">
                 {{ $slot }}
-            </div>
+                </div>
+                </main>
+            
             <!-- end main content -->
-        </div>
+        <!-- </div> -->
     </x-shared.general.main>
 </x-base-layout>
--- resources/views/layouts/stats-sidebar.blade.php
+++ resources/views/layouts/stats-sidebar.blade.php
@@ -2,19 +2,22 @@
     <livewire:pages.campus-information-card/>
     <x-shared.general.main class="mb-5">
         <livewire:pages.page-navigation/>
-        <div class="row mt-3 mx-0 px-0">
+        <!-- <div class="row mt-3 mx-0 px-0"> -->
             <!-- Left Sidebar start -->
             <livewire:pages.components.sidebar.stats-sidebar/>
             <!-- Left Sidebar End -->
             <!-- Main Content -->
-            <div class="col-12 col-md-7 px-0">
+            <!-- <div class="col-12 col-md-7 px-0"> -->
+                 <main class="main-wrapper">
+                    <div class="main-content">
                 {{ $slot }}
             </div>
+        </main>
             <!-- end main content -->
             <!-- RIGHT SIDEBAR START -->
             <livewire:pages.components.sidebar.right-sidebar/>
             <!-- RIGHT SIDEBAR CLOSED -->
-        </div>
+        <!-- </div> -->
         @push(AppConst::PUSH_JS)
             <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
         @endpush
