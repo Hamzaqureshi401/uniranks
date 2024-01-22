180ab5e7ef7bdba857b89ce3c544ad8a23da94e4
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2024-01-22 16:34:33 +0500

    half comp

--- resources/views/livewire/schools/fairs/career-talks-list.blade.php
+++ resources/views/livewire/schools/fairs/career-talks-list.blade.php
@@ -250,10 +250,92 @@
    <div class="card mt-3">
       <div class="p-3">
          <div class="row">
-            <a href="{{route('admin.schoolFairs.careerTalks.view',$fair->id)}}" class="col-12 col-lg-2 university-img-div">
-            <img src="{{$fair->school->logo_url ?? ''}}" class="" width="93px">
-            </a>
-            <div class="col-8 col-lg-8 ps-3 d-flex flex-column align-items-stretch" style="
+            <div class="col-md-2" style="width: 9.666667% !important;">
+               <div class="row">
+                  <div class="col-12">
+                     <a href="{{route('admin.schoolFairs.careerTalks.view',$fair->id)}}" class="university-img-div">
+                     <img src="{{$fair->school->logo_url ?? ''}}" class="" width="93px">
+                     </a>
+                  </div>
+                  <div class="col-12 mt-2">
+                     @if(!empty($fair->active_status))
+                     <span style="color: green; margin-left: 25px;">{{ 'Active' }}</span>
+                     @else
+                     <span style="color: red; margin-left: 25px;">{{ 'In Active' }}</span>
+                     @endif
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-7" style="
+               width: 60% !important;
+               border-right: 1px solid #ccc; /* Choose your desired border color */
+               ">
+               <div class="row mt-4" >
+                  <div class="col-lg-12">
+                     <a href="{{route('admin.schoolFairs.fair.view',$fair->id)}}" class="h5 blue text-decoration-none">{{$fair->school->school_name ?? ''}}</a>
+                  </div>
+                  <div class="col-lg-12 light-blue smallh  d-md-flex">
+                     <div
+                        class="">{{$fair->school?->country?->country_name.', '. $fair->school?->city?->city_name}} | @lang('Sessions: '){{ $fair->sessions->first()->id ?? '' }} | {{ $fair->start_date->format('n/j/Y gA') }} | @lang('Capacity: '){{ $fair->max }}@lang(' Universities')
+                     </div>
+                  </div>
+                   <div class="row">
+                  <div class="col-12">&nbsp</div>
+                  
+                  <div class="col-md-4">
+                     <span style="color: #ae1414;">{{ 'Deadline: ' }}{{ $fair->end_date->format('n/j/Y') }}</span>
+                  </div>
+                  <div class="col-md-4 light-blue">
+                     @lang('Remaining Seats') {{intVal($fair->sessions_count) - $fair->confirmed_universities_count}}
+                     / {{$fair->sessions_count}}
+                  </div>
+                  <div class="col-md-4">
+                     <div class="d-flex justify-content-end">
+                        @if(!empty($fair->active_status))
+                        <a href="{{ route('admin.schoolFairs.careerTalks.view', $fair->id) }}" style="width: 80% !important" class="button-sm button-blue m-0 w-100 text-decoration-none"
+                           wire:click="acceptFair('{{$fair->id}}')"> @lang('Book')</a>
+                        @else
+                        <button disabled class="button-sm button-disabled m-0 w-100" style="cursor: not-allowed; width: 80% !important">@lang('Book')</button>
+                        @endif
+                     </div>
+                  </div>
+               </div>
+               </div>
+            </div>
+            <div class="col-md-3 mt-4" style=" width: 30% !important;">
+               <div class="h6 mb-0 gray">
+                  <div class="row">
+                     <div class="col-md-12">@lang('Fees') {{$fair->school?->g_12_fee_range?->currency_range}}</div>
+                     <div class="col-md-12 mt-2">@lang('Grade 12 Students')
+                        :{{$fair->school?->number_grade12}}
+                     </div>
+                     <div class="col-md-12 mt-2">@lang('Grade 11 Students')
+                        :{{$fair->school?->number_grade11}}
+                     </div>
+                     <div x-data="{ showOtherMajors: false }" class="col-md-12 mt-2">
+                        @lang('Majors'): 
+                        @foreach($fair->majors->take(1) as $index => $major)
+                        {{ $major->title }}
+                        @if ($index < $fair->majors->count() - 1)
+                        ,
+                        @endif
+                        @endforeach
+                        @if ($fair->majors->count() > 2)
+                        ... 
+                        <span class="light-blue cursor-pointer" @click="showOtherMajors = !showOtherMajors">
+                        {{ $fair->majors->count() - 2 }} other {{ Str::plural('major', $fair->majors->count() - 2) }}
+                        </span>
+                        <div x-show="showOtherMajors" class="mt-2">
+                           @foreach($fair->majors->skip(1) as $major)
+                           {{ $major->title }},
+                           @endforeach
+                        </div>
+                        @endif
+                     </div>
+                  </div>
+               </div>
+            </div>
+           <!--  <div class="col-8 col-lg-8 ps-3 d-flex flex-column align-items-stretch" style="
                margin-left: -36px !important;
                border-right: 1px solid #ccc; /* Choose your desired border color */
                ">
@@ -273,69 +355,10 @@
                      </div>
                   </div>
                </div>
-            </div>
-            <div class="col-12 col-lg-2 ps-3 d-flex flex-column  align-items-stretch">
-               <div class="h6 mb-0 gray">
-                  <div class="col-md-12">@lang('Fees') {{$fair->school?->g_12_fee_range?->currency_range}}</div>
-                  <div class="col-md-12 mt-2">@lang('Grade 12 Students')
-                     :{{$fair->school?->number_grade12}}
-                  </div>
-                  <div class="col-md-12 mt-2">@lang('Grade 11 Students')
-                     :{{$fair->school?->number_grade11}}
-                  </div>
-                  <div x-data="{ showOtherMajors: false }" class="col-md-12 mt-2">
-                     @lang('Majors'): 
-                     @foreach($fair->majors->take(1) as $index => $major)
-                     {{ $major->title }}
-                     @if ($index < $fair->majors->count() - 1)
-                     ,
-                     @endif
-                     @endforeach
-                     @if ($fair->majors->count() > 2)
-                     ... 
-                     <span class="light-blue cursor-pointer" @click="showOtherMajors = !showOtherMajors">
-                     {{ $fair->majors->count() - 2 }} other {{ Str::plural('major', $fair->majors->count() - 2) }}
-                     </span>
-                     <div x-show="showOtherMajors" class="mt-2">
-                        @foreach($fair->majors->skip(1) as $major)
-                        {{ $major->title }},
-                        @endforeach
-                     </div>
-                     @endif
-                  </div>
-               </div>
-            </div>
+            </div> -->
+         
          </div>
-         <div class="row">
-            <div class="col-9">
-               <div class="row">
-                  <div class="col-md-2"> 
-                     @if(!empty($fair->active_status))
-                     <span style="color: green; margin-left: 25px;">{{ 'Active' }}</span>
-                     @else
-                     <span style="color: red; margin-left: 25px;">{{ 'In Active' }}</span>
-                     @endif
-                  </div>
-                  <div class="col-md-3 text-center">
-                     <span style="color: #ae1414;">{{ 'Deadline: ' }}{{ $fair->end_date->format('n/j/Y') }}</span>
-                  </div>
-                  <div class="col-md-3 light-blue text-end">
-                     @lang('Remaining Seats') {{intVal($fair->sessions_count) - $fair->confirmed_universities_count}}
-                     / {{$fair->sessions_count}}
-                  </div>
-                  <div class="col-md-4">
-                     <div class="d-flex justify-content-end">
-                        @if(!empty($fair->active_status))
-                        <a href="{{ route('admin.schoolFairs.careerTalks.view', $fair->id) }}" style="width: 80% !important" class="button-sm button-blue m-0 w-100 text-decoration-none"
-                           wire:click="acceptFair('{{$fair->id}}')"> @lang('Book')</a>
-                        @else
-                        <button disabled class="button-sm button-disabled m-0 w-100" style="cursor: not-allowed; width: 80% !important">@lang('Book')</button>
-                        @endif
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
+         
       </div>
    </div>
    @empty
